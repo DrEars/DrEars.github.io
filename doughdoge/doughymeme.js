dc972a446438373b6cb40dba77f031f4d7dd790c
Dominick Cataldi <dominickmakespie@gmail.com>
2016-10-17 19:05:32 -0400

    save

--- doughdoge/doughymeme.js
+++ doughdoge/doughymeme.js
@@ -1,1 +1,12 @@
-var game = new Phaser.Game(69,420);
+var game = new Phaser.Game(360,420);
+console.log(game) 
+class Boot {
+  preload() {
+    this.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = "true";
+    this.scale.pageAlignVertically = "true";
+  }
+}
+
+game.state.add("Boot",Boot);
+game.state.start("Boot");
