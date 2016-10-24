d1c46f5a6985c5a8d1c0f7e6866ca169de129709
Dominick Cataldi <dominickmakespie@gmail.com>
2016-10-24 19:09:46 -0400

    save

--- doughdoge/doughymeme.js
+++ doughdoge/doughymeme.js
@@ -7,18 +7,29 @@ class Boot {
     this.scale.pageAlignVertically = "true";
   }
   create() {
-    this.state.start("Load")
+    this.state.start("Load");
   }
 }
 
 class Load { 
   preload() { 
     console.log("Loading...............:3...");
+    this.load.image("Yes","assets/Yes.jpg");
   }
   create() {
     console.log("Loaded");
+    this.state.start("Play");
   }
 }
+
+class Play {
+  create() {
+    console.log("Entered Play ");
+    this.background = this.add.tileSprite(0,0,360,420,"Yes");
+    this.background.autoScroll(0,999);
+  }
+} 
 game.state.add("Boot",Boot);
 game.state.add("Load",Load);
+game.state.add("Play", Play);
 game.state.start("Boot");
