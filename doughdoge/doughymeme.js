951eedbc7321756de0a7d2b3e0e4c80a13a842af
Dominick Cataldi <dominickmakespie@gmail.com>
2016-11-28 18:20:59 -0500

    save

--- doughdoge/doughymeme.js
+++ doughdoge/doughymeme.js
@@ -14,9 +14,19 @@ var C = {
     "file": "assets/Lenny_Face.png",
     "width": 31,
     "height": 31,
-    "frames": 1
+    "frames": 1,
+    "startx": 180,
+    "starty": 210
+  } ,
+  "e": { 
+    "file": "assets/kek.png",
+    "width": 40,
+    "height": 64,
+    "frames": 1,
+    "startx": 200,
+    "starty": 210
   } 
-} 
+}
 var game = new Phaser.Game(C.cyes.width,C.cyes.height);
 var game = new Phaser.Game(C.cyes.width,C.cyes.height);
 var game = new Phaser.Game(C.cyes.width,C.cyes.height);
@@ -36,8 +46,10 @@ class Load {
   preload() { 
     console.log("Loading...............:3...");
     this.load.image("cyes",C.cyes.Yes);
+    this.load.image("enemy",C.e.file,C.e.width,C.e.height,C.e.frames);
     this.load.spritesheet("player",C.p.file,C.p.width,C.p.height,C.p.frames)
   }
+
   create() {
     console.log("Loaded");
     this.state.start("Play");
@@ -49,8 +61,15 @@ class Play {
     console.log("Entered Play ");
     this.background = this.add.tileSprite(0,0,C.cyes.width,C.cyes.height,"cyes");
     this.background.autoScroll(C.cyes.xspeed,C.cyes.yspeed);
+    this.player = this.add.sprite(C.p.startx,C.p.starty,"player");
+    this.player.anchor.set(0.5,0.5);
+    this.player.smoothed = false;
+    this.player.scale.set(4.20);
   }
-} 
+  update() { 
+    console.log("createCursorKeys() called.");
+  }
+}
 function restart() {
   game.state.start("Boot");
 }
